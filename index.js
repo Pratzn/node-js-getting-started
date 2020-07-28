2ee6d7d9cfac199ec0e0b7762e2fb926ed93a187
Pratz <pratz.nud@gmail.com>
2020-07-28 18:24:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+const ewelink = require('ewelink-api');
+const app = express()
+const connection = new ewelink({
+    email: 'prat.nud@icloud.com',
+    password: 'apm12ev8',
+    region: 'as',
+});
+app.get('/status', (req, res) => {
+    (async () => {
+    const status = await connection.getDevicePowerState('100022d6fe');
+    res.send(status)
+    })().catch((e) => {
+        console.log("error"+e.message);});
+})
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/toggle', (req, res) => {
+    (async () => {
+        const status = await connection.toggleDevice('100022d6fe');
+
+        res.send(status)
+    })().catch((e) => {
+        console.log("error"+e.message);});
+
+})
+
+app.listen(PORT, () => {
+    console.log('Start server at port '+PORT)
+})
